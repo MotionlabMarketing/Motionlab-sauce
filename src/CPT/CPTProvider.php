5d3e8673c53b5e4985bfd24cbe7e00e4d718adf3
Karl Fletcher <karl@motionlab.co.uk>
2020-02-26 10:36:23 +0000

    Moved ACF for CPTs into a new temporary setup. Used to be stored within blocks related to the CPT - needs its own handler similar to the block update we have just made

--- src/CPT/CPTProvider.php
+++ src/CPT/CPTProvider.php
@@ -23,6 +23,7 @@ class CPTProvider
     public function bootstrap()
     {
         $this->registerCustomPostTypes();
+        $this->registerCustomPostTypesAcf();
     }
 
     private function registerCustomPostTypes()
@@ -30,6 +31,11 @@ class CPTProvider
         foreach(self::$customPostTypes as $customPostType){
             $cptInstance = new $customPostType();
         }
+    }
+
+    private function registerCustomPostTypesAcf() {
+        die(pa(__DIR__ . '/cpt-acf.php'));
+        include __DIR__ . '/cpt-acf.php';
     }
 
     public static function orverrideCptClass(string $key, string $className)
