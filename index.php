b4a5df47121c8cfd38a84e7b601b5a5a8306c0a4
Chris Threlfall <chris@motionlab.co.uk>
2020-03-05 14:51:03 +0000

    adding in content wrap class to index.php

--- index.php
+++ index.php
@@ -4,45 +4,48 @@ get_header();
 
 ?>
 
-<section class="px4 py5 bg-white">
+<section class="content-wrap">
 
-    <main class="container clearfix">
+    <div class="px4 py5 bg-white">
 
-        <div class="col col-12">
-            <h1 class="text-center">
-                <?php if(isset($_GET['s'])) { echo "Site Search"; } else { echo get_the_title(get_option('page_for_posts')); } ?>
-            </h1>
-            <div class="page-search">
-                <?php get_search_form(); ?>
+        <main class="container clearfix">
+
+            <div class="col col-12">
+                <h1 class="text-center">
+                    <?php if(isset($_GET['s'])) { echo "Site Search"; } else { echo get_the_title(get_option('page_for_posts')); } ?>
+                </h1>
+                <div class="page-search">
+                    <?php get_search_form(); ?>
+                </div>
             </div>
-        </div>
 
-        <div class="clearfix">
-            <?php
+            <div class="clearfix">
+                <?php
 
-            if (have_posts()) :
-                $i = 0;
-                while (have_posts()) : the_post();
+                if (have_posts()) :
+                    $i = 0;
+                    while (have_posts()) : the_post();
 
-                    echo "<div class='col col-12 md-col-5 lg-col-4 md-hover-zoom'>";
-                    include('template-parts/post.php');
-                    echo "</div>";
-                    $i++;
+                        echo "<div class='col col-12 md-col-5 lg-col-4 md-hover-zoom'>";
+                        include('template-parts/post.php');
+                        echo "</div>";
+                        $i++;
 
-                endwhile;
-            endif;
+                    endwhile;
+                endif;
 
-            ?>
-        </div>
+                ?>
+            </div>
 
-        <div class="pagination my4">
-            <?php echo paginate_links(); ?>
-        </div>
+            <div class="pagination my4">
+                <?php echo paginate_links(); ?>
+            </div>
 
-    </main>
+        </main>
+
+    </div>
 
 </section>
-
 
 <?php
 
