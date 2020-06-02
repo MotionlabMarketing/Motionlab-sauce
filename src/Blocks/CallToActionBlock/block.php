9156c617095c37cbc3544bf0671844477afa9ac8
Karl Fletcher <karl@karlfletcher.co.uk>
2020-06-02 09:13:18 +0100

    Updated template to include wrapping div

--- src/Blocks/CallToActionBlock/block.php
+++ src/Blocks/CallToActionBlock/block.php
@@ -1,23 +1,25 @@
 <?php $this->setCallToAction($this->blockConfiguration['call_to_action']); ?>
 <section class="footer-installer <?php echo $this->callToAction['background_colour'] ? $this->callToAction['background_colour']  : ''; ?> mb4" <?php echo $this->getAttributeString() ?>>
 
-    <?php echo $this->callToAction['cta_image']['sizes']['medium']; ?>
+    <div class="content wysiwyg mx-auto relative <?php echo $this->callToAction['cta_bordered_content'] ? 'border border-lighten-10 cta-bordered' : ''; ?>">
+        <img src="<?php echo $this->callToAction['cta_image']['sizes']['medium']; ?>" />
 
-    <?php echo $this->callToAction['cta_content'] ?>
+        <?php echo $this->callToAction['cta_content'] ?>
 
-    <div data-element="buttons" data-button-alignment="<?php echo $this->callToAction['cta_button_alignment']; ?>">
-        <?php
-        if (!empty($this->callToAction['cta_buttons'])) :
-            foreach ($this->callToAction['cta_buttons'] as $index => $button) : ?>
+        <div data-element="buttons" data-button-alignment="<?php echo $this->callToAction['cta_button_alignment']; ?>">
+            <?php
+            if (!empty($this->callToAction['cta_buttons'])) :
+                foreach ($this->callToAction['cta_buttons'] as $index => $button) : ?>
 
-                <a data-element="button" data-button-style="<?php echo $button['button_type']; ?>" href="<?php echo $button['button']['url']; ?>">
-                    <?php echo $button['button']['title']; ?>
-                    <?php if (!empty($button['icon'])) : ?>
-                        <span class="h5 ml2"><?php echo $button['icon']; ?></span>
-                    <?php endif; ?>
-                </a>
+                    <a data-element="button" data-button-style="<?php echo $button['button_type']; ?>" href="<?php echo $button['button']['url']; ?>">
+                        <?php echo $button['button']['title']; ?>
+                        <?php if (!empty($button['icon'])) : ?>
+                            <span class="h5 ml2"><?php echo $button['icon']; ?></span>
+                        <?php endif; ?>
+                    </a>
 
-            <?php endforeach;
-        endif; ?>
+                <?php endforeach;
+            endif; ?>
+        </div>
     </div>
 </section>
