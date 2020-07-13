f5a711d2bac1db66693ae496a0a8cbd1afaea391
Karl Fletcher <karl@karlfletcher.co.uk>
2020-07-13 16:54:09 +0100

    Minor amendment to KF carousel layout

--- src/Blocks/KeyFeaturesBlock/carousel.php
+++ src/Blocks/KeyFeaturesBlock/carousel.php
@@ -6,8 +6,8 @@
 
         <div class="owl-carousel owl-theme carousel-<?php echo $this->getBlockPositionID(); ?>">
             <?php foreach ($this->blockConfiguration['features_feature'] as $feature):?>
-                <div class="js-match-height bg-white p4 py6 mb4 col-12 md-col-4 text-center width-100" style="border: .5rem solid #f2f2f2;" data-animation="zoom">
-                    <div class="flex items-center justify-center mb4" data-mh="icon-height" style="max-height: 50px;">
+                <div class="js-match-height bg-white p4 mb4 col-12 md-col-4 text-center width-100" style="border: .5rem solid #f2f2f2;" data-animation="zoom">
+                    <div class="flex items-center justify-center mb4" data-mh="icon-height" style="">
                         <?php if ($feature['icon_type'] === 'custom') : ?>
                             <?php
                             if ($feature['icon_image']['subtype'] == "svg+xml") : ?>
@@ -15,14 +15,14 @@
                                     <?php echo file_get_contents(get_attached_file($feature['icon_image']['id'])); ?>
                                 </div>
                             <?php else : ?>
-                                <img src="<?php echo $feature['icon_image']['sizes']['icon']; ?>" alt="<?php echo $feature['icon_image']['alt']; ?>" style="max-height: 64px; max-width: 64px;">
+                                <img src="<?php echo $feature['icon_image']['sizes']['medium']; ?>" alt="<?php echo $feature['icon_image']['alt']; ?>" style="max-height: 15rem;min-height: 15rem;">
                             <?php endif; ?>
                         <?php else: ?>
                             <span class="black h00 mb4"><?php echo $feature['feature_icon']; ?></span>
                         <?php endif; ?>
                     </div>
 
-                    <p class="h4 black uppercase"><?php echo $feature['feature_title']; ?></p>
+                    <p class="h4 black serif uppercase"><?php echo $feature['feature_title']; ?></p>
 
                     <div class="mx-auto px4" style="">
                         <?php echo $feature['feature_content']; ?>
