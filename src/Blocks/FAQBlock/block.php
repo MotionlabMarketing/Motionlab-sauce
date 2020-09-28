2d1410d641fc3bd5b3489cbc1300f3aee36a0dee
Joe Curran <jbird608@gmail.com>
2020-09-28 14:25:56 +0100

    Updated the FAQ Block

--- src/Blocks/FAQBlock/block.php
+++ src/Blocks/FAQBlock/block.php
@@ -9,8 +9,9 @@ FAQs: $this->blockConfiguration['faq_faqs']; - Repeater
 ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*|Block Settings|~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*/
 ?>
 
-<section class="px4 py5 <?php echo $this->blockConfiguration['background_colour'] ? $this->blockConfiguration['background_colour']  : ''; echo " " . $this->blockConfiguration['banner_background_image_position'] . " "; ?> <?php echo $this->blockConfiguration['banner_background_image_position']; ?> bg-cover height-100 zoom" <?php echo $this->getAttributeString() ?> style="background-image:url('<?php echo $this->blockConfiguration['banner_background_image']['url']; ?>')" data-aos="fade-in">
-    <div class="container clearfix relative flex flex-column <?php echo $this->blockConfiguration['faq_alignment'];?>">
+<section class="px4 py5 <?php echo $this->blockConfiguration['background_colour'] ? $this->blockConfiguration['background_colour']  : '';
+                        echo " " . $this->blockConfiguration['banner_background_image_position'] . " "; ?> <?php echo $this->blockConfiguration['banner_background_image_position']; ?> bg-cover height-100 zoom" <?php echo $this->getAttributeString() ?> style="background-image:url('<?php echo $this->blockConfiguration['banner_background_image']['url']; ?>')" data-aos="fade-in">
+    <div class="container clearfix relative flex flex-column <?php echo $this->blockConfiguration['faq_alignment']; ?>">
 
         <div class="col <?php echo $this->blockConfiguration['faq_width']; ?>">
 
@@ -19,27 +20,38 @@ FAQs: $this->blockConfiguration['faq_faqs']; - Repeater
                 <?php echo $this->blockConfiguration['faq_introduction']; ?>
             </div>
 
-            <?php foreach ((array) $this->blockConfiguration['faq_faqs'] as $item) : ?>
+            <?php if (!empty($this->blockConfiguration['faq_faqs'])) : ?>
+                <div class="accordion mt-6">
 
-                <div class="" data-accordion data-accordion-collection="<?php echo $this->getBlockPositionID(); ?>" data-accordion-status="closed">
-                    <div class="acc-head">
-                        <label for="" class="bold h4">
-                            <?php echo $item['faq_faqs_question']; ?>
-                        </label>
-                        <div class="acc-indicator">
-                            <span class="h"></span>
-                            <span class="v"></span>
+                    <?php foreach ($this->blockConfiguration['faq_faqs'] as $key => $row) : ?>
+                        <div data-accordion-collection="<?= get_the_id() ?>" data-accordion-active="false" class="w-100 py-3 cursor-pointer">
+
+                            <div class="acc-head">
+                                <label for="" class="h4">
+                                    <?php echo $row['faq_faqs_question']; ?>
+                                </label>
+                                <div class="acc-indicator plus">
+                                    <span class="h"></span>
+                                    <span class="v"></span>
+                                </div>
+                            </div>
+                            <div class="acc-body text-left" style="display: none;">
+
+                                <div class="body-content">
+                                    <?php echo $row['faq_faqs_answer']; ?>
+                                </div>
+
+                            </div>
+
                         </div>
-                    </div>
+                    <?php endforeach; ?>
 
-                    <div class="acc-body">
-
-                        <?php echo $item['faq_faqs_answer']; ?>
-
-                    </div>
                 </div>
+            <?php endif; ?>
 
-            <?php endforeach; ?>
+            <div class="faq-button-wrapper">
+                <a href="#" class="faq-button btn">View All</a>
+            </div>
         </div>
     </div>
 
