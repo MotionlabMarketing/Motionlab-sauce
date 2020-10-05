de8cd6f1b0e38cf3461b0fe3503501e017445c53
Matt Polke <matt.polke@motionlab.co.uk>
2020-10-05 17:24:52 +0100

    added new threes layout to testimonials

--- src/Blocks/TestimonialsBlock/threes.php
+++ src/Blocks/TestimonialsBlock/threes.php
@@ -11,27 +11,43 @@ $testimonials = $this->getTestimonials();
 ?>
 
 <section class="bg-light-grey py5" <?php echo $this->getAttributeString() ?> data-aos="fade-in">
-    <div class="js-testimonial-carousel pb4">
 
-        <?php
-        $i = 1;
-        $numTestimonials = count($testimonials);
-        ?>
-        <?php foreach ($testimonials as $testimonial) : ?>
-            <?php if(($i + 2) % 3 == 0): ?>
-                <div>
-            <?php endif; ?>
-            <article class="bg-white col-12 mx3 rounded overflow-hidden shadow mb4 md-mb0">
-                <div class="js-match-height p4 text-center flex items-center justify-center flex-column">
-                    <h3 class="lh2 bold"><?php echo $testimonial['title']; ?></h3>
-                    <div class=""><?php echo strip_empty_tags($testimonial['content']); ?></div>
-                    <p class="bold dark-purple mb0"><?php echo $testimonial['reviewer']; ?></p>
-                </div>
-            </article>
-            <?php if(($i) % 3 == 0 || $i == $numTestimonials): ?>
-                </div>
-            <?php endif; ?>
-            <?php $i++; ?>
-        <?php endforeach; ?>
+    <div class="container clearfix">
+        <div class="js-testimonial-carousel-new pb4">
+
+            <div>
+                <?php
+                $i = 1;
+                $numTestimonials = count($testimonials);
+                ?>
+
+                <?php foreach ($testimonials as $testimonial) : ?>
+                    <?php if(($i + 2) % 3 == 0): ?>
+                        <div>
+                    <?php endif; ?>
+                    <article class="col col-12 md-col-4 p4">
+                        <?php echo strip_empty_tags($testimonial['content']); ?>
+                    </article>
+                    <?php if(($i) % 3 == 0 || $i == $numTestimonials): ?>
+                        </div>
+                    <?php endif; ?>
+                    <?php $i++; ?>
+                <?php endforeach; ?>
+            </div>
+
+        </div>
     </div>
 </section>
+
+<script>
+    jQuery(document).ready(function($) {
+        $('.js-testimonial-carousel-new').slick({
+            centerMode: true,
+            initialSlide: 1,
+            arrows: false,
+            infinite: true,
+            dots: true,
+            slidesToShow: 1
+        });
+    });
+</script>
