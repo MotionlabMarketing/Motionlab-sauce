ac14d19225e16ad91301eec93d119f3e86250485
Joe Curran <jbird608@gmail.com>
2020-11-02 14:47:39 +0000

    Removed Hover on Mobile

--- src/Blocks/BlogBlock/block.php
+++ src/Blocks/BlogBlock/block.php
@@ -20,7 +20,7 @@
                     $p->category_id = $p->category_id ? $p->category_id : wp_get_post_categories($p->ID)[0];
                     $p->category_primary = get_term($p->category_id);
                 ?>
-                    <article class="p3 col-12 md-col-4 rounded hover-zoom mb3">
+                    <article class="p3 col-12 md-col-4 rounded md-hover-zoom mb3">
                         <div class="bg-white shadow overflow-hidden zoom">
                             <a href="<?php echo get_the_permalink($p->ID); ?>" class="overflow-hidden block">
                                 <div class="bg-charcoal overflow-hidden bg-cover bg-center" style="height: 16rem; background-image: url('<?php echo $p->featuredImage; ?>');"></div>
