7db38ffba7faa594a4d1eaf871ff543c5437248f
Joe Curran <jbird608@gmail.com>
2020-11-18 10:37:37 +0000

    Updated the background colour on listing template

--- src/Blocks/PackagesBlock/block_full.php
+++ src/Blocks/PackagesBlock/block_full.php
@@ -8,7 +8,7 @@ $packages = $this->getPackages();
 
         <?php foreach ($packages as $package) : ?>
             <div class="col col-12 md-col-6 p4" data-element="package" data-package-type="<?php echo (isset($package['is_featured']) && $package['is_featured'] == true) ? "featured" : "standard" ?>" data-mh="package">
-                <div class="p4 <?php echo $package['background_colour'] ?>">
+                <div class="p4 <?php echo (isset($package['background_colour'])) ? $package['background_colour'] : ""; ?>">
                     <h2 class="title white heading-font h2 max-width-16 <?php echo (!empty($package['subtitle'])) ? "mb2" : "mb3"; ?>"><?php echo $package['title']; ?></h2>
                     <?php if (!empty($package['subtitle'])) : ?>
                         <h3 class="mb3 white heading-font h4 uppercase"><?php echo $package['subtitle'] ?></h3>
@@ -25,13 +25,12 @@ $packages = $this->getPackages();
                         <p class="price mt4 mb2 heading-font h3 white"><?php echo $package['price'] ?></p>
                     <?php endif; ?>
                     <div>
-                        <a href="<?php echo $package['link_location'] ?>" data-element="button" class="button heading-font h3 bg-white hover-white <?php echo str_replace("bg-", "", $package['background_colour']['background_colour']); ?> hover-bg-aqua"><?php echo $package['link_title'] ?></a>
+                        <a href="<?php echo $package['link_location'] ?>" data-element="button" class="button heading-font h3 bg-white hover-white <?php echo str_replace("bg-", "", $package['background_colour']); ?> hover-bg-aqua"><?php echo $package['link_title'] ?></a>
                     </div>
                 </div>
             </div>
         <?php endforeach; ?>
 
     </div>
-
 
 </section>
