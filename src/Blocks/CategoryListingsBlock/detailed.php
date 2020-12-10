f6d26e5f458143017f20ee4c370b4e43c86694d3
Joe Curran <jbird608@gmail.com>
2020-12-10 15:29:42 +0000

    Updated the sizing of categorylisting block

--- src/Blocks/CategoryListingsBlock/detailed.php
+++ src/Blocks/CategoryListingsBlock/detailed.php
@@ -1,7 +1,7 @@
 <section class="<?php echo $this->blockConfiguration['background_colour'] ? $this->blockConfiguration['background_colour']  : 'bg-light-grey'; ?> clearfix px4 py5 lg-mx0" <?php echo $this->getAttributeString() ?> data-aos="fade-in">
     <div class="container">
         <div class="flex items-center justify-center">
-            <?php if (!empty(trim($this->title))): ?>
+            <?php if (!empty(trim($this->title))) : ?>
                 <h2><?php echo $this->title ?></h2>
             <?php endif; ?>
         </div>
@@ -13,7 +13,7 @@
             <?php foreach ($this->taxonomyTerms as $term) : ?>
                 <?php $icon = get_field('partner_sector_icon', $term) ?>
 
-                <div class="col col-12 md-col-3 p2 mb3" data-mh="category-panel">
+                <div class="col col-12 md-col-3 p2 mb3">
                     <div class="rounded overflow-hidden box-shadow">
                         <div class="px3 pt3 bg-white">
                             <div class="flex items-center justify-center mb3" data-mh="category-inner">
@@ -21,8 +21,8 @@
                             </div>
                             <p class="bold light-blue text-center" data-mh="partner-title"><?php echo $term->name; ?></p>
                         </div>
-                        <div class="p3 bg-light-blue" data-mh="category-content">
-                            <div class="wysiwyg white">
+                        <div class="p3 bg-light-blue">
+                            <div class="wysiwyg white" data-mh="category-content">
                                 <?php if (!empty($term->description)) : ?>
                                     <?php echo $term->description; ?>
                                 <?php else : ?>
@@ -35,7 +35,7 @@
                                 $cptPermalink = get_post_type_archive_link($this->postType);
                                 $cptAssociatedPage = get_field('partner_sector_associated_page', $term);
                                 $outputLink = $cptPermalink;
-                                if(isset($cptAssociatedPage) && $cptAssociatedPage !== "") :
+                                if (isset($cptAssociatedPage) && $cptAssociatedPage !== "") :
                                     $outputLink = $cptAssociatedPage;
                                 endif;
                                 ?>
